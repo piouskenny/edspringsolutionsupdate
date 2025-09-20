8644928c9d2e94034b101da500f002b0e0baf753
Piouskenny <kehindeu13@gmail.com>
2025-09-20 14:17:08 +0100

    updated logo

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -3,7 +3,7 @@ import Port2 from "../assets/portfolios/Port2.png";
 import Port3 from "../assets/portfolios/Port3.png";
 import Port4 from "../assets/portfolios/port4.png";
 import Port5 from "../assets/portfolios/port5.png";
-import Port6 from "../assets/portfolios/port6.png";
+// import Port6 from "../assets/portfolios/port6.png";
 import Port7 from "../assets/portfolios/port7.png";
 import Port8 from "../assets/portfolios/port8.jpg";
 import Port9 from "../assets/portfolios/port9.jpg";
@@ -53,13 +53,13 @@ const Partners = () => {
           <div className="col-1">
             <img className="w-fill h-fill" src={Port2} alt="Partner 2" />
           </div>
-          <div className="col-1">
+          {/* <div className="col-1">
             <img
               className="md:w-40 md:h-40 h-[60px] w-[100px] object-center"
               src={Port6}
               alt="Partner 1"
             />
-          </div>
+          </div> */}
           <div className="col-1">
             <img
               className="md:w-40 md:h-40 h-[60px] w-[100px] object-center"
